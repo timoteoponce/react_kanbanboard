9cb6f7a76b7c3b1e04f17f1f3cde464be71669f5
Timoteo Ponce <timoteo.ponce@swissbytes.ch>
2016-03-15 16:11:11 -0400

    Up to page 90

--- app/App.js
+++ app/App.js
@@ -3,12 +3,37 @@ import styles from './App.css';
 
 class App extends Component{
   render() {
+    var place = "Santa Cruz";
     return (
       <div className={styles.root}>
-        <h1>Hello World</h1>
+        <h1>Hello World Timoteo Ponce at {place}</h1>
+        <GroceryList/>
       </div>
     );
   }
 }
 
-export default App
+class GroceryList extends Component{
+  render(){
+    return (
+      <ul>
+        <ListItem quantity="1" name="Bread"/>
+        <ListItem quantity="5" name="Eggs"/>
+        <ListItem quantity="3" name="Milk"/>
+        <ListItem quantity="5" name="Meat"/>
+      </ul>
+    );
+  }
+}
+
+class ListItem extends Component{
+  render(){
+    return (
+      <li>
+      {this.props.quantity} x {this.props.name}
+      </li>
+    );
+  }
+}
+
+export default App 
